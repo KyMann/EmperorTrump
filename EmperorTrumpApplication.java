17267d4490cfcbe8984d4724af870f411e8b2b94
Kyle Mann <akavir7@gmail.com>
2017-06-27 17:16:56 -0500

    started web page, databases, and streaming

--- EmperorTrumpApplication.java
+++ EmperorTrumpApplication.java
@@ -2,37 +2,98 @@ package com.KyMann.EmperorTrump;
 
 
 
+import com.KyMann.EmperorTrump.Models.EmperorTweet;
+import com.KyMann.EmperorTrump.Models.TrumpTweet;
+import com.KyMann.EmperorTrump.Models.data.EmperorTweetsDao;
+import com.KyMann.EmperorTrump.Models.data.TrumpTweetsDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import twitter4j.Paging;
-import twitter4j.Status;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import com.KyMann.EmperorTrump.Models.Controllers.EmperorControl;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 @SpringBootApplication
 public class EmperorTrumpApplication {
 
-	public static void main(String[] args) throws TwitterException {
-        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-        configurationBuilder.setDebugEnabled(true)
-                .setOAuthConsumerKey("1PmXuppMVlMdz4O2u1Fjk76tD")
-                .setOAuthConsumerSecret("dXlUnXsKkwTyCjguVyHzCA3unRulV7fxG3flncsFZJC3miCT1Q")
-                .setOAuthAccessToken("1758677539-OZiWn1VgJwhtedqLNVnrGAg3mJthneFtmqt2jT5")
-                .setOAuthAccessTokenSecret("zMNoj5n4hZ9CotaQyXlMqVKNR0ZDBvrjgw7elRvCldH2q");
+    private static ArrayList<EmperorTweet> emperorTweetsArrayList = new ArrayList<EmperorTweet>();
+    @Autowired
+    private static EmperorTweetsDao emperorTweetsDao;
+    private static TrumpTweetsDao trumpTweetsDao;
 
+	public static void main(String[] args) throws TwitterException {
+
+	    //Config builder sets up twitter communications
+	    ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.setDebugEnabled(true)
+                .setOAuthConsumerKey(TwitterKeys.consumerKey)
+                .setOAuthConsumerSecret(TwitterKeys.consumerSecret)
+                .setOAuthAccessToken(TwitterKeys.accessTokenKey)
+                .setOAuthAccessTokenSecret(TwitterKeys.accessTokenSecret);
+        //twitter factory is the java lib object for twitter4java
         TwitterFactory tf = new TwitterFactory(configurationBuilder.build());
         twitter4j.Twitter twitter = tf.getInstance();
 
+        //twitter streaming input
+        StatusListener listener = new StatusListener(){
+            public void onStatus(Status status) {
+                TrumpTweet tweet = new TrumpTweet(status.getText());
+                trumpTweetsDao.save(tweet);
+            }
+            public void onStallWarning(StallWarning stallWarning) {}
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+            public void onScrubGeo(long longitund, long latitude) {}
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+
+        //pageNum controls pages back into tweet history, might need to increment at some point, certain how to prevent repeats as tweets move back through the list
+        //count is how many tweets per page, max 100
         int pageNum = 1;
         Paging paging = new Paging(pageNum,100);
-
+        //.get is past tweets
         List<Status> status=twitter.getUserTimeline("@realDonaldTrump", paging);
+
+        //TODO: set up old tweets database, should only be run once
+
+        //EmperorControl object needed to run conversion methods
+        EmperorControl empTrumpTranslator = new EmperorControl();
+
+        //for loop loops through past tweets here
         for(Status s:status) {
-            System.out.println(s.getUser().getName() + " " + s.getText());
+
+            //this runs the converter - DOES NOT RETURN STRING BUT OBJECT
+            EmperorTweet empTweet = empTrumpTranslator.insertEmperorWords(s.getText());
+
+            //
+            emperorTweetsArrayList.add(empTweet);
+            if (empTweet.getChanges() != 0) {
+                //System.out.println(s.getUser().getName() + " " + s.getText());
+                //System.out.println(empTweet.getTweet() + ", " +  String.valueOf(empTweet.getChanges()));
+                //TODO: send to empertweets database
+            }
+
+        }
+
+        //TODO:start up web page
+        SpringApplication.run(EmperorTrumpApplication.class, args)
+
+        //TODO: check occasionally and post(and remove from database) approved tweets using set interval
+        for (EmperorTweet tweet : emperorTweetsDao.findAll()) {
+            if (tweet.isApproved()) {
+                try {
+                    twitter.updateStatus(tweet.getTweet());
+                } catch (TwitterException e) {
+                    System.err.println("Error occurred while posting status!");
+                }
+            }
         }
     }
 
---
+++ Models/TrumpTweet.java
@@ -0,0 +1,21 @@
+package com.KyMann.EmperorTrump.Models;
+
+/**
+ * Created by Kyle on 6/20/2017.
+ */
+public class TrumpTweet {
+
+    private String tweet;
+
+    public TrumpTweet(String tweet) {
+        this.tweet = tweet;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(String tweet) {
+        this.tweet = tweet;
+    }
+}
