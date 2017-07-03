033eb798c7b4d2c82af13150f45b330d76cdcf5b
Kyle <akavir7@gmail.com>
2017-07-03 15:51:52 -0500

    trying to make git work
    also finished twitter posting and gradle update

---
+++ src/main/java/com/KyMann/EmperorTrump/Models/EmperorTweet.java
@@ -0,0 +1,47 @@
+package com.KyMann.EmperorTrump.Models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by Kyle on 6/15/2017.
+ */
+public class EmperorTweet {
+
+    private String tweet;
+
+    private int changes;
+
+    private boolean approved;
+
+    @Autowired
+    private int Id;
+
+    public EmperorTweet() {}
+
+    public EmperorTweet(String aTweet, int aChanges) {
+        super();
+        tweet = aTweet;
+        changes = aChanges;
+        approved = false;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(String tweet) {
+        this.tweet = tweet; }
+
+    public int getChanges() {
+        return changes;
+    }
+
+    public void setChanges(int changes) {
+        this.changes = changes;
+    }
+
+    public boolean isApproved() {return approved;}
+
+    public int getId() {return Id;}
+
+}
