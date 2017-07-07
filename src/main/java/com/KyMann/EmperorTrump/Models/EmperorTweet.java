896aca0831bca333684435aeb6735f7ef6bca7ab
Kyle Mann <akavir7@gmail.com>
2017-07-07 16:14:18 -0500

    Dependencies working

---
+++ src/main/java/com/KyMann/EmperorTrump/Models/EmperorTweet.java
@@ -0,0 +1,44 @@
+package com.KyMann.EmperorTrump.Models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmperorTweet {
+
+    private String tweet;
+
+    private int changes;
+
+    private boolean approved;
+
+    @Autowired
+    private int Id;
+
+    public EmperorTweet() {}
+
+    public EmperorTweet(String aTweet, int aChanges) {
+        super();
+        tweet = aTweet;
+        changes = aChanges;
+        approved = false;
+    }
+
+    public String getTweet() {
+        return tweet;
+    }
+
+    public void setTweet(String tweet) {
+        this.tweet = tweet; }
+
+    public int getChanges() {
+        return changes;
+    }
+
+    public void setChanges(int changes) {
+        this.changes = changes;
+    }
+
+    public boolean isApproved() {return approved;}
+
+    public int getId() {return Id;}
+
+}
